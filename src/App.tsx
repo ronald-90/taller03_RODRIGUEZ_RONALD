f910315ef873578f0cebddf047cc1f6f3883e1af
rrodrigu71 <rrodrigu71@cuc.edu.co>
2024-09-30 04:54:21 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miImagen from "./assets/img/Imagen_car_png.png"
+
+function App() {
+
+  return (
+    <>
+      <div> 
+      <img src={miImagen} alt="logo car" />
+      <h1>Mustang</h1>
+      <p>El Mustang ha ofrecido a lo largo de su historia diferentes versiones, como el Mustang gt y Mustang shelby</p>  
+      </div>  
+    </>
+  )
+}
+
+export default App
